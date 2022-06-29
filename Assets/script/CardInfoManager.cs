0b738510953d8ee3dbffd48098a1d30660df7a05
HAL <mattyan.ktkr@gmail.com>
2022-06-30 08:24:45 +0900

    Code clean

--- Assets/script/CardInfoManager.cs
+++ Assets/script/CardInfoManager.cs
@@ -11,8 +11,7 @@ public class CardInfoManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InitializeSearchCardList();
-        
+        InitializeSearchCardList();      
     }
 
     // Update is called once per frame
