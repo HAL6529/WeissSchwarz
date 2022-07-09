f1a31b0a26ce026368eec4ed890b9c8a112bad88
HAL <mattyan.ktkr@gmail.com>
2022-07-09 17:48:32 +0900

    Add Function of Photon

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -17,4 +17,47 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    // ロビーへ入室完了
+    void OnJoinedLobby()
+    {
+        // どこかのルームへ入室
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    // ロビーへの入室が失敗
+    void OnFailedToConnectToPhoton()
+    {
+
+    }
+
+    // ルームへ入室失敗
+    void OnPhotonRandomJoinFailed()
+    {
+        // 自分でルームを作成
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    // 無事にルームへ入室
+    void OnJoinedRoom() {
+
+    }
+
+    // PhotonRealTimeと接続が切れた場合
+    void OnConnectionFail()
+    {
+
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        if (pauseStatus)
+        {
+            PhotonNetwork.Disconnect();
+        }
+        else
+        {
+
+        }
+    }
 }
