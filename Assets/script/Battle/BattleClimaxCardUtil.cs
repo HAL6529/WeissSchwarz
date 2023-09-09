311c1154ea71501649782507012bc6086f588837
HAL6529 <mattyan.ktkr@gmail.com>
2023-09-09 14:01:03 +0900

    Add MainPower Util

--- Assets/script/Battle/BattleClimaxCardUtil.cs
+++ Assets/script/Battle/BattleClimaxCardUtil.cs
@@ -5,29 +5,32 @@ using UnityEngine.UI;
 
 public class BattleClimaxCardUtil : MonoBehaviour
 {
+    [SerializeField] BattleModeGuide m_BattleModeGuide;
+
     public Image image;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private BattleModeCard m_BattleModeCard = null;
 
     public void SetClimax(BattleModeCard climax)
     {
+        m_BattleModeCard = climax;
+
         if (climax == null)
         {
             image.color = new Color(1, 1, 1, 0 / 255);
             image.sprite = null;
             return;
         }
+
         image.color = new Color(1, 1, 1, 255 / 255);
         image.sprite = climax.sprite;
+    }
+
+    public void onClick()
+    {
+        if (m_BattleModeCard == null)
+        {
+            return;
+        }
+        m_BattleModeGuide.showImage(m_BattleModeCard);
     }
 }
