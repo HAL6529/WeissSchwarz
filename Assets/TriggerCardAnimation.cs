28e4bf13b4bb7e63a0f55771645c509273a736e9
HAL6529 <mattyan.ktkr@gmail.com>
2023-10-08 21:00:22 +0900

    TriggerAnimation

--- Assets/TriggerCardAnimation.cs
+++ Assets/TriggerCardAnimation.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityForge.AnimCallbacks;
 
 public class TriggerCardAnimation : MonoBehaviour
 {
     [SerializeField] Image m_Image;
     [SerializeField] GameManager m_GameManager;
     [SerializeField] Animator m_Animator;
+
+    private void Start()
+    {
+        m_Animator.AddClipCallback(0, "TriggerCheck", 0.2f, () => GetDeckTopSprite());
+        m_Animator.AddClipCallback(0, "TriggerCheck", 1.1f, () => AnimationEnd());
+    }
 
     public void Play()
     {
@@ -17,8 +24,16 @@ public class TriggerCardAnimation : MonoBehaviour
         m_Animator.Play("TriggerCheck", 0, 0.0f);
     }
 
-    public void GetDeckTopSprite()
+    private void GetDeckTopSprite()
     {
+        Debug.Log("GetDeckTopSprite");
         m_Image.sprite = m_GameManager.myDeckList[0].sprite;
+    }
+
+    private void AnimationEnd()
+    {
+        Debug.Log("AnimationEnd");
+        this.gameObject.SetActive(false);
+        m_GameManager.isAnimation = false;
     }
 }
