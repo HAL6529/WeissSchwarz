613a15ba986a573631000ba8aabe9053b543ac89
HAL6529 <mattyan.ktkr@gmail.com>
2025-04-24 00:08:22 +0900

    Add EndUtil

--- Assets/script/Battle/WinAndLose.cs
+++ Assets/script/Battle/WinAndLose.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class WinAndLose : MonoBehaviour
 {
     [SerializeField] Text text;
     [SerializeField] DialogManager m_DialogManager;
     [SerializeField] NotEraseDialog m_NotEraseDialog;
+    [SerializeField] StrixManager m_StrixManager;
+    bool flg = false;
 
     public void Win()
     {
+        if (flg)
+        {
+            return;
+        }
+        flg = true;
         m_DialogManager.CloseAllDialog();
         m_NotEraseDialog.OffDialog();
         this.gameObject.SetActive(true);
@@ -19,9 +27,23 @@ public class WinAndLose : MonoBehaviour
 
     public void Lose()
     {
+        if (flg)
+        {
+            return;
+        }
+        flg = true;
         m_DialogManager.CloseAllDialog();
         m_NotEraseDialog.OffDialog();
         this.gameObject.SetActive(true);
         text.text = "Lose";
+    }
+
+    public void onHomeBtn()
+    {
+        if(m_StrixManager != null)
+        {
+            m_StrixManager.onExit();
+        }
+        SceneManager.LoadScene("RoomSelect");
     }
 }
