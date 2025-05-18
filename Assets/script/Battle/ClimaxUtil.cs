cd5d202bcd367f08fe61b9f964b197f719bcfece
HAL6529 <mattyan.ktkr@gmail.com>
2025-05-18 16:53:06 +0900

    AddPersona3Climax

--- Assets/script/Battle/ClimaxUtil.cs
+++ Assets/script/Battle/ClimaxUtil.cs
@@ -14,9 +14,12 @@ public class ClimaxUtil
             case EnumController.CardNo.DC_W01_19T:
             case EnumController.CardNo.LB_W02_10T:
             case EnumController.CardNo.LB_W02_20T:
+            case EnumController.CardNo.P3_S01_14T:
                 return EnumController.ClimaxType.POWER_THOUSAND_AND_SOUL_ONE;
             case EnumController.CardNo.DC_W01_20T:
             case EnumController.CardNo.LB_W02_11T:
+            case EnumController.CardNo.P3_S01_13T:
+            case EnumController.CardNo.P3_S01_20T:
                 return EnumController.ClimaxType.SOUL_PLUS_TWO;
             default:
                 return EnumController.ClimaxType.VOID;
